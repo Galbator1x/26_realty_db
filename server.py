11f4acd9058236241962f45cf5bdf37d23895597
Ivan Filimonov <snake73w@gmail.com>
2017-02-23 23:26:56 +0700

    change the way ads filtering

--- server.py
+++ server.py
@@ -11,25 +11,25 @@ db.init_app(app)
 
 @app.route('/', methods=['GET'])
 def ads_list():
-    filter_conditions = [Ad.active == True]
+    ads = Ad.query.filter(Ad.active == True)
     district = request.args.get('oblast_district')
-    min_price = request.args.get('min_price')
-    max_price = request.args.get('max_price')
+    min_price = request.args.get('min_price', None, type=int)
+    max_price = request.args.get('max_price', None, type=int)
     new_building = request.args.get('new_building')
     page = request.args.get('page', 1, type=int)
     per_page = 10
 
     if district:
-        filter_conditions.append(Ad.oblast_district == district)
+        ads = ads.filter(Ad.oblast_district == district)
     if min_price:
-        filter_conditions.append(Ad.price >= min_price)
+        ads = ads.filter(Ad.price >= min_price)
     if max_price:
-        filter_conditions.append(Ad.price <= max_price)
+        ads = ads.filter(Ad.price <= max_price)
     if new_building:
-        filter_conditions.append(Ad.under_construction == True or
-                Ad.construction_year >= date.today().year - 2)
+        ads = ads.filter(Ad.under_construction == True or
+                         Ad.construction_year >= date.today().year - 2)
 
-    ads = Ad.query.filter(*filter_conditions).paginate(page, per_page, False)
+    ads = ads.paginate(page, per_page, False)
     return render_template('ads_list.html',
             ads=ads,
             district=district,
